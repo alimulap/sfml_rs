86770706684959e0345f186843cc0d78496ca849
MohammadAlimul <mohammad4limul@gmail.com>
2023-04-17 20:37:40 +0700

    test window

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
+use sfml::{
+    window::{ContextSettings, Style, Event, Key}, 
+    graphics::{RenderWindow, RenderTarget, Color}
+};
+
 fn main() {
-    println!("Hello, world!");
+    let settings = ContextSettings {
+        antialiasing_level: 2,
+        ..Default::default()
+    };
+
+    let mut window = RenderWindow::new(
+        (480, 640),
+        "SFML",
+        Style::CLOSE,
+        &settings,
+    );
+
+    loop {
+        while let Some(event) = window.poll_event() {
+            match event {
+                Event::Closed
+                | Event::KeyPressed { 
+                    code: Key::Escape,..
+                } => return,
+                _ => (),
+            }
+        }
+
+        window.clear(Color::WHITE);
+        window.display();
+    }
 }
